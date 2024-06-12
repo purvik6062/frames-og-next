c4fd642fde2d728ca4f98d2c192188f811566acd
purvik6062 <purvikpanchal6336@gmail.com>
2024-06-12 15:12:22 +0530

    change style component

--- src/app/api/images/og/ccTest/route.tsx
+++ src/app/api/images/og/ccTest/route.tsx
@@ -3,6 +3,8 @@ import { ImageResponse } from "next/og";
 import Image from "next/image";
 import profile from "@/app/api/images/og/assets/profile-img.png";
 import texture from "@/app/api/images/og/assets/meta2.png";
+import main from "@/app/api/images/og/assets/main.jpg";
+
 export const revalidate = 0;
 
 export const runtime = "edge";
@@ -24,145 +26,125 @@ export async function GET(req: NextRequest) {
   //     new URL("../assets/Inter-Bold.ttf", import.meta.url)
   //   ).then((res) => res.arrayBuffer());
 
-  const bg = await fetch(
-    new URL("../assets/main-bg.jpg", import.meta.url)
-  ).then((res) => res.arrayBuffer());
+  const main = await fetch(new URL("../assets/main.jpg", import.meta.url)).then(
+    (res) => res.arrayBuffer()
+  );
 
-  const profile = await fetch(
-    new URL("../assets/profile-img.png", import.meta.url)
-  ).then((res) => res.arrayBuffer());
+  //   const profile = await fetch(
+  //     new URL("../assets/profile-img.png", import.meta.url)
+  //   ).then((res) => res.arrayBuffer());
 
-  const texture = await fetch(
-    new URL("../assets/meta2.jpg", import.meta.url)
-  ).then((res) => res.arrayBuffer());
+  //   const texture = await fetch(
+  //     new URL("../assets/meta2.jpg", import.meta.url)
+  //   ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
     (
       <div
-        // tw="bg-cover h-full"
         style={{
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          //   padding: "100px 180px",
+          height: "100%",
+          width: "100%",
         }}
       >
         <img
-          /*@ts-ignore */
-          src={bg}
-          style={{ position: "absolute", padding: "100px 180px" }}
-          //   tw="bg-cover h-full"
+          /*@ts-ignore*/
+          src={main}
+          style={{
+            position: "absolute",
+            zIndex: -1,
+          }}
           alt="background"
         />
         <div
           style={{
-            backgroundColor: "rgba(255, 255, 255, 0.74)",
-            borderRadius: "78px",
             display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            width: "100%",
-            height: "100%",
-            padding: "40px",
-            margin: "20px 40px",
+            flexDirection: "column",
+            paddingTop: "10%",
+            paddingLeft: "17%",
           }}
         >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              paddingLeft: "10px",
-            }}
-          >
-            <div style={{ display: "flex" }}>
-              <img
-                /*@ts-ignore */
-                src={profile}
-                alt="Profile"
-                style={{
-                  width: "200px",
-                  height: "200px",
-                  borderRadius: "50%",
-                }}
-              />
-            </div>
-            <div style={{ textAlign: "left", display: "flex" }}>
-              <div
-                style={{
-                  color: "black",
-                  fontSize: "60px",
-                  marginBottom: "10px",
-                  display: "flex",
-                }}
-              >
-                You are invited to delegate your Voting Power on
-              </div>
-              <div style={{ display: "flex", gap: "10px" }}>
-                <div
-                  style={{
-                    padding: "10px 20px",
-                    borderRadius: "9999px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "#0500FF",
-                    color: "white",
-                    fontSize: "35px",
-                  }}
-                >
-                  {dao_name}
-                </div>
-                <span
-                  style={{
-                    display: "flex",
-                    alignSelf: "center",
-                    color: "black",
-                    fontSize: "40px",
-                  }}
-                >
-                  to
-                </span>
-                <div
-                  style={{
-                    padding: "10px 20px",
-                    borderRadius: "9999px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "#0500FF",
-                    color: "white",
-                    fontSize: "35px",
-                  }}
-                >
-                  {address}
-                </div>
-              </div>
-            </div>
-          </div>
           <div style={{ display: "flex" }}>
             <img
-              /*@ts-ignore*/
-              src={texture}
-              alt="Chora Club"
+              /*@ts-ignore */
+              //   src={profile}
+              src="https://gateway.lighthouse.storage/ipfs/QmUsAYs3PwJoetYko6iDN36t43W9xWXYFCjAk1ahq9CBfA"
+              alt="Profile"
               style={{
-                borderRadius: "60px",
-                height: "100%",
+                width: "120px",
+                height: "120px",
+                borderRadius: "50%",
               }}
             />
+          </div>
+          <div
+            style={{
+              textAlign: "left",
+              display: "flex",
+              flexDirection: "column",
+              width: "400px",
+              fontWeight: 600,
+            }}
+          >
+            <div
+              style={{
+                color: "black",
+                fontSize: "32px",
+                paddingTop: "30px",
+                paddingBottom: "30px",
+                display: "flex",
+              }}
+            >
+              You are invited to delegate your Voting Power on
+            </div>
+            <div style={{ display: "flex", gap: "10px" }}>
+              <div
+                style={{
+                  padding: "10px 30px",
+                  borderRadius: "9999px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#0500FF",
+                  color: "white",
+                  fontSize: "28px",
+                }}
+              >
+                {dao_name}
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignSelf: "center",
+                  color: "black",
+                  fontSize: "30px",
+                  paddingLeft: "10px",
+                  paddingRight: "10px",
+                }}
+              >
+                to
+              </div>
+              <div
+                style={{
+                  padding: "10px 30px",
+                  borderRadius: "9999px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#0500FF",
+                  color: "white",
+                  fontSize: "28px",
+                }}
+              >
+                {address}
+              </div>
+            </div>
           </div>
         </div>
       </div>
     ),
     {
       ...size,
-      //   fonts: [
-      //     {
-      //       name: "Inter",
-      //       data: await interSemiBold,
-      //       style: "normal",
-      //       weight: 400,
-      //     },
-      //   ],
     }
   );
 }
